9c13fc9992ee27db03808a7de1839e9c71a384f8
vivek koyani <41565769+VIVEKKOYANI@users.noreply.github.com>
2022-07-19 00:06:50 +0530

    add setup Route in frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<h1>Product Listing Component</h1>} />
+          <Route path='/add' element={<h1>Add Product Component</h1>} />
+          <Route path='/update' element={<h1>Update Product Component</h1>} />
+          <Route path='/logout' element={<h1>Logout Component</h1>} />
+          <Route path='/profile' element={<h1>Profile Component</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
